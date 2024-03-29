b8aa9b1d3a94dc95d7568fe9d408b35549b01357
Ronny Lam <ronlam@hnw.nu>
2024-03-29 14:05:40 +0100

    DST rewrite that survives powercycles

--- gardenled.ino
+++ gardenled.ino
@@ -20,6 +20,7 @@
   int sunChecked;
   int GNMrise;
   int GNMset;
+  int DST = 1;
   const int pwmFull = 127;
   const int pwmHigh = 32;
   const int pwmLow = 1;
@@ -63,7 +64,7 @@ unsigned long ledTime = 0;
 
     
     //init and get the time
-    configTime(3600, 0, ntpServer);
+    configTime(3600, DST*3600, ntpServer);
 
     server.on("/xxx", handle_OnConnect);
     server.on("/xxx/ledfull", handle_ledFull);
@@ -92,44 +93,33 @@ unsigned long ledTime = 0;
     getLocalTime(&timeinfo);
     
     if (sunChecked != (timeinfo.tm_mon+1) * (timeinfo.tm_mday+1) && timeinfo.tm_hour > 3) {
-          if (timeinfo.tm_yday < 84 || timeinfo.tm_yday > 304) {
-            configTime(3600, 0, ntpServer);
+          if (timeinfo.tm_yday > 90 && timeinfo.tm_yday < 298) {DST = 1;}
+          else if (timeinfo.tm_yday >= 84 && timeinfo.tm_yday <= 90) {
+            if (timeinfo.tm_wday == 6){DST = 0;}
+            else if (timeinfo.tm_yday == 84 && timeinfo.tm_wday - (timeinfo.tm_yday - 84) == -1){DST = 1;}
+            else if (timeinfo.tm_yday == 85 && timeinfo.tm_wday - (timeinfo.tm_yday - 85) == -1){DST = 1;}
+            else if (timeinfo.tm_yday == 86 && timeinfo.tm_wday - (timeinfo.tm_yday - 86) == -1){DST = 1;}
+            else if (timeinfo.tm_yday == 87 && timeinfo.tm_wday - (timeinfo.tm_yday - 87) == -1){DST = 1;}
+            else if (timeinfo.tm_yday == 88 && timeinfo.tm_wday - (timeinfo.tm_yday - 88) == -1){DST = 1;}
+            else if (timeinfo.tm_yday == 89 && timeinfo.tm_wday - (timeinfo.tm_yday - 89) == -1){DST = 1;}
+            else if (timeinfo.tm_yday == 90 && timeinfo.tm_wday - (timeinfo.tm_yday - 90) == -1){DST = 1;}
+            else {DST = 0;}
           }
-          if (timeinfo.tm_yday > 90 && timeinfo.tm_yday < 298) {
-            configTime(3600, 3600, ntpServer);
+          else if (timeinfo.tm_yday >= 298 && timeinfo.tm_yday <= 304) {
+            if (timeinfo.tm_wday == 6){DST = 0;}
+            else if (timeinfo.tm_yday == 298 && timeinfo.tm_wday - (timeinfo.tm_yday - 298) == -1){DST = 0;}
+            else if (timeinfo.tm_yday == 299 && timeinfo.tm_wday - (timeinfo.tm_yday - 299) == -1){DST = 0;}
+            else if (timeinfo.tm_yday == 300 && timeinfo.tm_wday - (timeinfo.tm_yday - 300) == -1){DST = 0;}
+            else if (timeinfo.tm_yday == 301 && timeinfo.tm_wday - (timeinfo.tm_yday - 301) == -1){DST = 0;}
+            else if (timeinfo.tm_yday == 302 && timeinfo.tm_wday - (timeinfo.tm_yday - 302) == -1){DST = 0;}
+            else if (timeinfo.tm_yday == 303 && timeinfo.tm_wday - (timeinfo.tm_yday - 303) == -1){DST = 0;}
+            else if (timeinfo.tm_yday == 304 && timeinfo.tm_wday - (timeinfo.tm_yday - 304) == -1){DST = 0;}
+            else {DST = 1;}
           }
-          if (timeinfo.tm_yday >= 84 && timeinfo.tm_yday <= 90) {
-            if (timeinfo.tm_yday == 84 && timeinfo.tm_wday - (timeinfo.tm_yday - 84) == -1){
-              configTime(3600, 3600, ntpServer);}
-            else if (timeinfo.tm_yday == 85 && timeinfo.tm_wday - (timeinfo.tm_yday - 85) == -1){
-              configTime(3600, 3600, ntpServer);}
-            else if (timeinfo.tm_yday == 86 && timeinfo.tm_wday - (timeinfo.tm_yday - 86) == -1){
-              configTime(3600, 3600, ntpServer);}
-            else if (timeinfo.tm_yday == 87 && timeinfo.tm_wday - (timeinfo.tm_yday - 87) == -1){
-              configTime(3600, 3600, ntpServer);}
-            else if (timeinfo.tm_yday == 88 && timeinfo.tm_wday - (timeinfo.tm_yday - 88) == -1){
-              configTime(3600, 3600, ntpServer);}
-            else if (timeinfo.tm_yday == 89 && timeinfo.tm_wday - (timeinfo.tm_yday - 89) == -1){
-              configTime(3600, 3600, ntpServer);}
-            else {configTime(3600, 3600, ntpServer);}
-          }
-          if (timeinfo.tm_yday >= 298 && timeinfo.tm_yday <= 304) {
-            if (timeinfo.tm_yday == 298 && timeinfo.tm_wday - (timeinfo.tm_yday - 298) == -1){
-              configTime(3600, 0, ntpServer);}
-            else if (timeinfo.tm_yday == 299 && timeinfo.tm_wday - (timeinfo.tm_yday - 299) == -1){
-              configTime(3600, 0, ntpServer);}
-            else if (timeinfo.tm_yday == 300 && timeinfo.tm_wday - (timeinfo.tm_yday - 300) == -1){
-              configTime(3600, 0, ntpServer);}
-            else if (timeinfo.tm_yday == 301 && timeinfo.tm_wday - (timeinfo.tm_yday - 301) == -1){
-              configTime(3600, 0, ntpServer);}
-            else if (timeinfo.tm_yday == 302 && timeinfo.tm_wday - (timeinfo.tm_yday - 302) == -1){
-              configTime(3600, 0, ntpServer);}
-            else if (timeinfo.tm_yday == 303 && timeinfo.tm_wday - (timeinfo.tm_yday - 303) == -1){
-              configTime(3600, 0, ntpServer);}
-            else {configTime(3600, 0, ntpServer);}
-          }
-          GNMrise = GNM.sunrise(timeinfo.tm_year+1900, timeinfo.tm_mon+1, timeinfo.tm_mday, timeinfo.tm_isdst);
-          GNMset = GNM.sunset(timeinfo.tm_year+1900, timeinfo.tm_mon+1, timeinfo.tm_mday, timeinfo.tm_isdst);
+          else {DST = 0;}
+          configTime(3600, DST*3600, ntpServer);
+          GNMrise = GNM.sunrise(timeinfo.tm_year+1900, timeinfo.tm_mon+1, timeinfo.tm_mday, DST);
+          GNMset = GNM.sunset(timeinfo.tm_year+1900, timeinfo.tm_mon+1, timeinfo.tm_mday, DST);
           sunChecked = (timeinfo.tm_mon+1) * (timeinfo.tm_mday+1);
     }
 
